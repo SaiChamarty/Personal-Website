f247f62583207d16c7b2cc66a46916daf6335a77
Sai Ganesh Chamarty <123051270+SaiChamarty@users.noreply.github.com>
2025-01-02 18:52:08 -0800

    Setup for navbar and App.js is completed. Next step is to refine the Navbar as seen in the Figma

---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+   return (
+       <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+   );
+};
+
+export default Navbar;
+
+
---
+++ src/saiborg.jsx
@@ -0,0 +1,5 @@
+const Saiborg = () => {
+    return <h1>Saiborg</h1>;
+};
+
+export default Saiborg;
